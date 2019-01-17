9cc486babc28044e72aee288d627b83ac6bbd832
Nicholas Alberts <nwalberts5@gmail.com>
2019-01-17 17:45:29 -0500

    fix

--- app/models/review.rb
+++
@@ -1,6 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :book
-
-  validates :rating, presence: true
-  validates :body, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  root "books#index"
 
-  resources :books, only: [:index, :show] do
-    resources :reviews, only: [:new, :create]
-  end
 end
